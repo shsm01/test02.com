38626f367fcd83cbea76596964435c13215a495c
shsm01 <shsm01@yandex.ru>
2017-05-24 14:28:17 +0400

    Change CODE file index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
-<?
+п»ї<?
 
 echo "TimeZone: ".date_default_timezone_get ()."<br>";
-echo "Сегодня: ".date("H:i:s d-M-Y")."<br>";
+echo "РЎРµРіРѕРґРЅСЏ: ".date("H:i:s d-M-Y")."<br>";
 
 phpinfo();
 
